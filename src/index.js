b06affd6a43008f9a162b62a661068107bb576a1
Hendrik Cammann <hendrikcammann@Hendriks-MBP-2.fritz.box>
2017-11-20 22:26:27 +0100

    [Exploded Map] finished it

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as d3 from "d3";
 
 import createJson from './modules/createJson';
 import pickGradientColor from './modules/pickGradientColor';
-import updateMapsPositions from './modules/updateMapPositions';
+import updateMapPositions from './modules/updateMapPositions';
 
 
 const DATASET = createJson();
@@ -21,49 +21,60 @@ const PI = config.circles.pi;
 const P = PI * Math.pow(RADIUS, 2);
 
 const POSITIONS = config.views;
+const MAP_POSITIONS = config.views.positions;
+const MAP = d3.select('#map-complete');
 
 const TRANSITION_TIME = config.transition.time;
 
 let isExploded = false;
 
 document.getElementById("toggle").addEventListener("click", toggleExploded);
+setupMap();
 
 function toggleExploded() {
   isExploded = !isExploded;
   if(!isExploded) {
-    updateMapsPositions(POSITIONS.normal, TRANSITION_TIME);
+    MAP.transition().attr("transform", "translate(" + MAP_POSITIONS.x + "," + MAP_POSITIONS.y + ")").duration(TRANSITION_TIME);
+    updateMapPositions(POSITIONS.normal, TRANSITION_TIME);
   } else {
-    updateMapsPositions(POSITIONS.exploded, TRANSITION_TIME);
+    MAP.transition().attr("transform", "translate(" + MAP_POSITIONS.x/2 + "," + MAP_POSITIONS.y/2 + ")").duration(TRANSITION_TIME);
+    updateMapPositions(POSITIONS.exploded, TRANSITION_TIME);
   }
 }
 
-for (let i = 0; i < DATASET.data.length; i++) {
-  setTimeout(function(){
-    document.getElementById('year').innerHTML = DATASET.data[i].year;
-    for (let j = 0; j < DATASET.data[i].districts.length; j++) {
-        let percentage = DATASET.data[i].districts[j].data.residents * POPULATION_FACTOR;
-        let percentageStudents = DATASET.data[i].districts[j].data.students * STUDENTS_FACTOR;
+function setupMap() {
+  for (let i = 0; i < DATASET.data.length; i++) {
+    setTimeout(function(){
+      document.getElementById('year').innerHTML = DATASET.data[i].year;
+      for (let j = 0; j < DATASET.data[i].districts.length; j++) {
+          //let percentage = DATASET.data[i].districts[j].data.residents * POPULATION_FACTOR;
+          let percentageStudents = DATASET.data[i].districts[j].data.students * STUDENTS_FACTOR;
 
-        let p = (P / MAX_PERCENTAGE) * percentage; //for size population
-        let pStudents = (P / MAX_PERCENTAGE) * percentageStudents; //for size students
+          //let p = (P / MAX_PERCENTAGE) * percentage; //for size population
+          let pStudents = (P / MAX_PERCENTAGE) * percentageStudents; //for size students
 
-        let gradientPosition = parseFloat((DATASET.data[i].districts[j].data.students * STUDENTS_FACTOR)) / MAX_PERCENTAGE;
-        let gradientColor = pickGradientColor(COLOR1, COLOR2, gradientPosition);
+          let gradientPosition = parseFloat((DATASET.data[i].districts[j].data.students * STUDENTS_FACTOR)) / MAX_PERCENTAGE;
+          let gradientColor = pickGradientColor(COLOR1, COLOR2, gradientPosition);
 
-        let radius = Math.sqrt((p / PI)); //for size population
-        let radiusStudents = Math.sqrt((pStudents / PI)); //for size students
+          //let radius = Math.sqrt((p / PI)); //for size population
+          let radiusStudents = Math.sqrt((pStudents / PI)); //for size students
 
-        let districtCircle = d3.select('#' + DATASET.data[i].districts[j].name.toLowerCase());
-        let districtOutline = d3.select('#' + DATASET.data[i].districts[j].name.toLowerCase() + '_out');
+          let districtCircle = d3.select('#' + DATASET.data[i].districts[j].name.toLowerCase());
+          let districtOutline = d3.select('#' + DATASET.data[i].districts[j].name.toLowerCase() + '_out');
 
-        if(!isExploded) {
-          updateMapsPositions(POSITIONS.normal, TRANSITION_TIME);
+          if(!isExploded) {
+            MAP.transition().attr("transform", "translate(" + MAP_POSITIONS.x + "," + MAP_POSITIONS.y + ")").duration(TRANSITION_TIME);
+            districtCircle.transition().attr('cx', POSITIONS.normal.circles[j].cx).attr('cy', POSITIONS.normal.circles[j].cy).duration(TRANSITION_TIME);
+            districtOutline.transition().attr("d", POSITIONS.normal.districts[j].d).duration(TRANSITION_TIME);
 
-        } else {
-          updateMapsPositions(POSITIONS.exploded, TRANSITION_TIME);
+          } else {
+            MAP.transition().attr("transform", "translate(" + MAP_POSITIONS.x/2 + "," + MAP_POSITIONS.y/2 + ")").duration(TRANSITION_TIME);
+            districtCircle.transition().attr('cx', POSITIONS.exploded.circles[j].cx).attr('cy', POSITIONS.exploded.circles[j].cy).duration(TRANSITION_TIME);
+            districtOutline.transition().attr("d", POSITIONS.exploded.districts[j].d).duration(TRANSITION_TIME);
+          }
+          districtCircle.style('fill', gradientColor);
+          districtCircle.transition().attr('r', radiusStudents).duration(TRANSITION_TIME);
         }
-        districtCircle.style('fill', gradientColor);
-        districtCircle.transition().style('r', radiusStudents).duration(TRANSITION_TIME);
-      }
-  }, 0 + (3000*i));
+    }, 0 + (3000*i));
+  }
 }
--- src/modules/updateMapPositions.js
+++ src/modules/updateMapPositions.js
@@ -4,8 +4,8 @@ export default function updateMapPositions(data, transitonTime) {
   for(let j = 0; j < data.districts.length; j++) {
     let districtCircle = d3.select('#' + data.districts[j].name.toLowerCase());
     let districtOutline = d3.select('#' + data.districts[j].name.toLowerCase() + '_out');
+    districtCircle.transition().attr('cx', data.circles[j].cx).attr('cy', data.circles[j].cy).duration(transitonTime);
     districtOutline.transition().attr("d", data.districts[j].d).duration(transitonTime);
-    districtCircle.style('cx', data.circles[j].cx);
-    districtCircle.style('cy', data.circles[j].cy);
+    //districtCircle.transition().style('cy', data.circles[j].cy);
   }
 }
